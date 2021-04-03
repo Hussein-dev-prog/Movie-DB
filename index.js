7c19d75c50b5ab5884413e5c1b6e069840df62ac
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-04-03 23:34:56 +0300

    Step 4

--- index.js
+++ index.js
@@ -19,3 +19,31 @@ app.get('/time', (req, res) => {
     res.send({ status: 200, message: hour + ":" + seconds })
 
 });
+
+//hello with id
+app.get('/hello/:id', (req, res) => {
+    res.status(200).send("Hello, " + req.params.id)
+})
+
+
+//search
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        const response = {
+            status: 200, message: "ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status: 500, error: true, message: "you have to provide a search"
+        };
+
+
+        res.status(500).send(response);
+
+    }
+});
