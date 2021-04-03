135ed3370d2b7a8f02e5293836f7b62199d7f3a3
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-04-03 23:04:25 +0300

    Step 3

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`OK`)
 })
+app.get('/test', (req, res) => {
+    res.send({ status: 200, message: "ok" })
+});
+app.get('/time', (req, res) => {
+    const date = new Date();
+    const hour = date.getHours();
+    var seconds = date.getSeconds();
+    res.send({ status: 200, message: hour + ":" + seconds })
+
+});
